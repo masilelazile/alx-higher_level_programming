5f6ff6c5dc81c0b29cda3b2b3afc9a666fa61ef0
masilelazile <masilelazile@gmail.com>
2023-08-14 20:54:16 +0000

    element at

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,8 @@
+my_list = [10, 20, 30, 40, 50]
+index = 2
+result = element_at(my_list, index)
+
+if result is not None:
+  print(f"The element at index {index} is: {result}")
+else:
+  print(f"Invalid index {index}")

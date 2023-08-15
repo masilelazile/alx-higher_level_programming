ed5cf6112db323b3fab313faf81b8a9cee4e2679
masilelazile <masilelazile@gmail.com>
2023-08-15 17:26:12 +0000

    print python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -5,20 +5,21 @@ lib = ctypes.CDLL('./libPyList.so')
 lib.print_python_list_info.argtypes = [ctypes.py_object]
 
 l = ['hello', 'World']
-lib.print_python_list_info(l)
+
+lib.print_python_list_info(1)
 
 del l[1]
-lib.print_python_list_info(l)
+lib.print_python_list_info(1)0
 
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+l += [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
 lib.print_python_list_info(l)
 
 l = []
 lib.print_python_list_info(l)
 
+# Append elements to the list
 l.append(0)
 lib.print_python_list_info(l)
-
 l.append(1)
 l.append(2)
 l.append(3)

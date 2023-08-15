164e75ba4589c8ec0669f89682009d32ef6fddec
masilelazile <masilelazile@gmail.com>
2023-08-15 17:06:19 +0000

    print python

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,33 +1,46 @@
+#define PY_SSIZE_T_CLEAN
 #include <stdio.h>
+#include <Python.h>
+#include <listobject.h>
+#include <object.h>
 
-unsigned long long factorial(unsigned int n) 
+/**
+ * print_python_list_info - Print basic info about Python lists.
+ * @p: PyObject.
+ * Return: nothing.
+ */
+
+void print_python_list_info(PyObject *p)
 {
-    if (n == 0 || n == 1) 
+    Py_ssize_t size, i;
+    size = PyList_GET_SIZE(p);
+
+    printf("[*] Size of the Python List = %ld\n", size);
+    printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+    for (i = 0; i < size; i++)
     {
-        return 1;
-    } 
-    else 
-    {
-        return n * factorial(n - 1);
+        PyObject *item = PyList_GET_ITEM(p, i);
+        printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
     }
 }
 
-int main() 
+int main(void)
 {
-    unsigned int num;
-    
-    printf("Enter a positive integer: ");
-    scanf("%u", &num);
-    
-    if (num < 0) 
-    {
-        printf("Factorial is not defined for negative numbers.\n");
-    } 
-    else 
-    {
-        unsigned long long result = factorial(num);
-        printf("Factorial of %u is %llu\n", num, result);
-    }
-    
+    Py_Initialize();
+
+    PyObject *pyList = PyList_New(5);
+    PyList_SetItem(pyList, 0, PyLong_FromLong(10));
+    PyList_SetItem(pyList, 1, PyFloat_FromDouble(3.14));
+    PyList_SetItem(pyList, 2, PyUnicode_DecodeUTF8("Hello", 5, "strict"));
+    PyList_SetItem(pyList, 3, PyBool_FromLong(1));
+    PyList_SetItem(pyList, 4, PyTuple_Pack(2, PyLong_FromLong(1), PyLong_FromLong(2)));
+
+    print_python_list_info(pyList);
+
+    Py_DECREF(pyList);
+
+    Py_Finalize();
+
     return 0;
 }

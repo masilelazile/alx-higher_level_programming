dc3daf33226877641cd1f91c12a29abc7f42e02d
masilelazile <masilelazile@gmail.com>
2023-08-15 00:17:15 +0000

    5-no_c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -5,7 +5,3 @@ def no_c(my_string):
         if char != 'c' and char != 'C':
             new_string += char
     return new_string
-
-input_string = input("Enter a string: ")
-result = no_c(input_string)
-print("String with 'c' and 'C' removed:", result)

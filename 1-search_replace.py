7853dd7fb83d818364ef1752999aae51a20ce34f
masilelazile <masilelazile@gmail.com>
2023-08-16 20:16:10 +0000

    more structure

--- 1-search_replace.py
+++ 1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-def search_replace(my_list, search, replace):
-    def s_r_elm(elm):
-        return elm if elm != search else replace
-    return list(map(s_r_elm, my_list))
+def square_matrix_simple(matrix=[]):
+    return [list(map(lambda x: x ** 2, row)) for row in matrix]

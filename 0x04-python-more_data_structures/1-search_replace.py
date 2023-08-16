655304d05bc52d265d012ec8ca6110275cfe967b
masilelazile <masilelazile@gmail.com>
2023-08-16 22:08:15 +0000

    more structure

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
-earch_replace(my_list, search, replace):
-    new_list = []
-    for element in my_list:
-        if element == search:
-            new_list.append(replace)
-        else:
-            new_list.append(element)
+#!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    new_list = [replace if element == search else element for element in my_list]
     return new_list
+
+original_list = [1, 2, 3, 2, 4, 2, 5]
+new_list = search_replace(original_list, 2, 99)
+print(new_list)

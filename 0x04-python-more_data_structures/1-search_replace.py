f95a99aee8e62afcab061c898d9ea6518e346da0
masilelazile <masilelazile@gmail.com>
2023-08-18 23:18:22 +0000

    pyth

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = [replace if element == search else element for element in my_list]
-    return new_list
-
-original_list = [1, 2, 3, 2, 4, 2, 5]
-new_list = search_replace(original_list, 2, 99)
-print(new_list)
+    n_list = []
+    for num in range(len(my_list)):
+        n_list.append(my_list[num])
+        if my_list[num] == search:
+            n_list[num] = replace
+    return n_list

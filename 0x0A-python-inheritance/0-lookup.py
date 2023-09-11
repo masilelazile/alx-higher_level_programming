7ea03712ffa08d37c9a5aaf18eae2a900c49a2ef
masilelazile <masilelazile@gmail.com>
2023-09-11 21:40:02 +0000

    look

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-"""
-    Module loopup function
-"""
+""" lookup py """
 
 
 def lookup(obj):
-    """ Retorna todos los atributos y métodos de un objeto
-
-    Args:
-        obj: una clase como objeto heredado
-
-    Returns: dir de objeto, lista
-    """
+    """ looks up """
     return dir(obj)

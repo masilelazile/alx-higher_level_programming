8cc372d6b1955dc90df78c02096d1acf2f6de504
masilelazile <masilelazile@gmail.com>
2023-12-03 16:49:33 -0800

    net

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Use curl to send a request and display the size of the response body in bytes
-size=$(curl -sI "$1" | grep -i Content-Length | awk '{print $2}' | tr -d '\r\n')
-
-# Check if Content-Length header is present in the response
-if [ -z "$size" ]; then
-  echo "Unable to determine the size of the response body."
-else
-  echo "$size"
-fi
-
+# Get the byte size of the HTTP response header for a given URL.
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2

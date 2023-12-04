d634709aacd422264d034460061803032a8acfc3
masilelazile <masilelazile@gmail.com>
2023-12-03 19:52:57 -0800

    saranghae

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
+"""displays the value of the X-Request-Id variable found in
+the header of the res.
 """
-Python script that sends a request to the URL and
-displays:
-- The body of the response if there are no errors
-- The error code when there is an HTTP error.
-"""
-import requests
-import sys
-
 
 if __name__ == "__main__":
-    r = requests.get(sys.argv[1])
-    if r.status_code >= 400:
-        print("Error code: {}".format(r.status_code))
+    from requests import get
+    from sys import argv
+
+    response = get(argv[1])
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
     else:
-        print(r.text)
+        print(response.text)
